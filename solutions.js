bfce223d3e06cbbf93b2f2ed0c34f3b0c67e9acd
Sofy <sofymurillo400@gmail.com>
2025-08-18 01:42:20 -0500

    Sofy Prada

--- solutions.js
+++ solutions.js
@@ -1,44 +1,45 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  let number1 = a;
-  let number2 = b;
-  return number1 + number2;
+   if (typeof a !== "number" || typeof b !== "number") {
+    console.log("Los datos de entrada no son adecuados");
+    return;
+  }
+  return a + b;
 }
+
 
 
 // Factorial of a Number
 function factorial(n) {
-  if ( n<0) {
-    return undefined;
+   if (typeof n !== "number" || n < 0) {
+    console.log("Los datos de entrada no son adecuados");
+    return;
   }
-  else if (n == 0){
-    return 1;
-  } else{
-    return n*factorial(n-1);
-  }
+  if (n === 0) return 1;
+   return n * factorial(n - 1);
 }
 
 
 // Find the Largest Number
 function findLargest(arr) {
-  arr.sort((a, b) => a - b); 
-  findLargest = arr[arr.length - 1];
-  return findLargest;
+  if (!Array.isArray(arr) || arr.some((e) => typeof e !== "number")) {
+    console.log("Los datos de entrada no son adecuados");
+    return;
+  }
+  return Math.max(...arr);
 }
 
 
 // Count Vowels in a String
 function countVowels(str) {
-  let count =0;
-  let vowels= "aeiouAEIOU";
-  for (let char of str){
-    if (vowels.includes(char)){
-      count++;
-    }
-    else{
-      continue;
-    }
+  if (typeof str !== "string") {
+    console.log("Los datos de entrada no son adecuados");
+    return;
   }
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let char of str) {
+    if (vowels.includes(char)) count++;  }
   return count;
   
 }
@@ -46,14 +47,15 @@ function countVowels(str) {
 
 // Check if a Number is Prime
 function isPrime(n) {
-  if (n <= 1) return false;
-  let raiz = Math.floor(Math.sqrt(n));
-  for (let i = 2; i <= raiz; i++){
-    if(n%i === 0){
-      return false;
-    }
-    return true;
+  if (typeof n !== "number" || n <= 1) {
+    console.log("Los datos de entrada no son adecuados");
+    return;
   }
+  let raiz = Math.floor(Math.sqrt(n));
+  for (let i = 2; i <= raiz; i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 
